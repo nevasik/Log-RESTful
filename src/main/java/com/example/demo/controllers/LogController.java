6d50685ff1896cc187f9f18ee6668b8be8f08115
nevasik <nevasik@mail.ru>
2023-04-30 14:32:51 +0300

    Final version!!!

--- src/main/java/com/example/demo/controllers/LogController.java
+++ src/main/java/com/example/demo/controllers/LogController.java
@@ -3,13 +3,11 @@ package com.example.demo.controllers;
 import com.example.demo.models.Log;
 import com.example.demo.services.LogService;
 import com.example.demo.util.JsonError;
-import com.example.demo.util.LogErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
 
 import jakarta.validation.Valid;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -18,7 +16,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
